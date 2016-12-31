3f60c21ce41d243bb114c5a15eee9fc54b83b9e5
kbriggs <kabriggs991@gmail.com>
2016-12-31 11:38:46 -0500

    Added code to wire up buttons with toast messages

--- app/src/main/java/edu/cofc/geoquiz/MainActivity.java
+++ app/src/main/java/edu/cofc/geoquiz/MainActivity.java
@@ -2,12 +2,41 @@ package edu.cofc.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the resource and set the listener
+        trueButton = (Button) findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make a Toast message when the button is pushed
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Find the resource and set the listener
+        falseButton = (Button) findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make a Toast message when the button is pushed
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
