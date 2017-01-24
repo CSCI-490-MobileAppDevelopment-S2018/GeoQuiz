89881fad7cdb19535ba7be7caba5a191a4700d4b
kbriggs <kabriggs991@gmail.com>
2017-01-24 17:26:47 -0500

    added image and button programmatically

--- app/src/main/java/edu/cofc/geoquiz/controller/MainActivity.java
+++ app/src/main/java/edu/cofc/geoquiz/controller/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -65,6 +67,12 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+        ImageButton imagebutton = new ImageButton(MainActivity.this);
+        imagebutton.setMaxWidth(13);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+        layout.addView(imagebutton);
 
     }
 
